42bf2f274d159397339430884f79f67a9639ab8d
wadetheriver <wadetheriver@gmail.com>
2014-12-29 21:44:48 -0800

    prime the date column for the raw data by populating all dates

--- prime_dates.php
+++ prime_dates.php
@@ -1,9 +1,15 @@
 <?php
+date_default_timezone_set("America/Los_Angeles"); //need to set this in php.ini cuz I keep getting this error
+
+//The Raw data is missing some days (technical errors and such
+//This will populate the date column with all dates
+
 $startDate = new DateTime('2001-01-12');
 $endDate = new DateTime('2010-05-26');
-$oneDay = new DateInterval('P1D');
+$oneDay = new DateInterval('P1D'); //knows days in month and if is a leap year. One day interval
 try {
     $db = new PDO('mysql:host=localhost;dbname=pendoreille', 'pendadmin', 'lynda');
+    //bind variables (named parameter) in prepared statement..
     $stmt = $db->prepare('INSERT INTO environmental_data (date_recorded) VALUE (:date)');
     while ($startDate <= $endDate) {
         /* Use the PDO bindValue() method instead of bindParam() to avoid strict error messages.
@@ -12,8 +18,10 @@ try {
         $stmt->bindValue(':date', $startDate->format('Y-m-d'));
         $stmt->execute();
         $startDate->add($oneDay);
+
+        echo $startDate->format('Y-m-d') . "Done <br/>";
     }
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-echo 'Done';
+echo 'All Done!';
