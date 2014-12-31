9d18739c139d17f37792068d38c2cb32ec567465
wadetheriver <wadetheriver@gmail.com>
2014-12-30 23:37:33 -0800

    successfuly got remainder of 2010 into DB using get_individual_files and OneDay class.

--- get_individual_files.php
+++ get_individual_files.php
@@ -10,36 +10,38 @@ use MyFoundationphp\PendOreille\OneDay;
 try {
     $db = new PDO('mysql:host=localhost;dbname=pendoreille', 'pendadmin', 'lynda');
     $table = 'environmental_data';
+
+
     //raw data for entire years stored in single text files ends on 5/27/2010
     //last full day was 5/26
     //days are now stored in single text files
     //simple usage of class, last few days of month of may
-    $month = 5; //may
-    $days = range(27,31);
-    foreach($days as $day){
-        $pend = new OneDay(new DateTime("2010/$month/$day"), $db, $table);
-        //why storing messages in array unless adding new array elements here?
-        //iterate multi dim array below
-
-        $messages[] = $pend->getMessages();
-    }
-
-
-//    $months = range(6, 12);
-//    $months30 = array(4, 6, 9, 11);
-//    foreach ($months as $month) {
-//        if (in_array($month, $months30)) {
-//            $days = range(1, 30);
-//        } elseif ($month == 2) {
-//            $days = range(1, 28);
-//        } else {
-//            $days = range(1, 31);
-//        }
-//        foreach ($days as $day) {
-//            $pend = new OneDay(new DateTime("2010/$month/$day"), $db, $table);
-//            $messages[] = $pend->getMessages();
-//        }
+//    $month = 5; //may
+//    $days = range(27,31);
+//    foreach($days as $day){
+//        $pend = new OneDay(new DateTime("2010/$month/$day"), $db, $table);
+//        //why storing messages in array unless adding new array elements here?
+//        //iterate multi dim array below
+//
+//        $messages[] = $pend->getMessages();
 //    }
+
+    // get the rest of the year from june to december
+    $months = range(6, 12);
+    $months30 = array(4, 6, 9, 11); //months with 30 days
+    foreach ($months as $month) {
+        if (in_array($month, $months30)) {
+            $days = range(1, 30);
+        } elseif ($month == 2) {
+            $days = range(1, 28);
+        } else {
+            $days = range(1, 31);
+        }
+        foreach ($days as $day) {
+            $pend = new OneDay(new DateTime("2010/$month/$day"), $db, $table);
+            $messages[] = $pend->getMessages();
+        }
+    }
 } catch(Exception $e) {
     $error = $e->getMessage();
 
