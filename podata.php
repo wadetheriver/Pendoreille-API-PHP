df2efc50513cb8baf0e275b0562513c360efff1a
wadetheriver <wadetheriver@gmail.com>
2015-01-01 21:16:46 -0800

    Verify submitted start and end dates, formed sql queries for submitted start and end dates

--- podata.php
+++ podata.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("America/Los_Angeles");
 // Initialize variables
 $start = null;
 $end = null;
@@ -28,5 +29,27 @@ function verifyDate($date) {
         return false;
     } else {
         return "$year-$month-$day";
+    }
+}
+
+$yesterday = new DateTime("yesterday");
+if(!$start) {
+    //store errors in array, need to send as JSON
+    $error = array('error' => 'Invalid Request. Permitted paramaters are startDate and endDate in YYYYMMDD format');
+
+} elseif ($start > $yesterday->format("Y-m-d"))  {
+    $error = array('error' => 'Start date cannot be greater than yesterday');
+
+} elseif ($start < '2001-01-12' || $end && $end < '2001-01-12') {
+    $error = array('error' => 'Out of range. No data available before 20010112.');
+} elseif ($start && $startOnly) {
+    //just get one day
+    $sql = "SELECT * FROM environmental_data WHERE date_recorded = '$start'";
+} elseif ($start && $end) {
+    if ($end < $start) {
+        $error = array('error' => 'End date is before start date.');
+    } else {
+        $sql = "SELECT * FROM environmental_data WHERE date_recorded
+                BETWEEN '$start' AND '$end'";
     }
 }
